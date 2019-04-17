c408d342c2d0e153434a04d57bfbb3800ef1ea11
jumperjqw <394319168@qq.cpm>
2019-04-17 21:07:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/test/FirstActivity.java
@@ -0,0 +1,50 @@
+package com.example.test;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+//活动（界面）1
+/*public class FirstActivity extends AppCompatActivity {
+
+   /* @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1=(Button) findViewById(R.id.button_1);//获取按钮Id
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent("com.example.activitytest.ACTION_START");
+                intent.addCategory("com.example.activitytest.My=Y_CATEGORY");
+                //Intent intent=new Intent(Intent.ACTION_VIEW);
+                //intent.setData(Uri.parse("http://www.baidu.com"));//跳转到网页或操作界面
+                startActivity(intent);//点击跳转
+            }
+        });
+     }
+     public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.add_item:
+            Toast.makeText(this, "you clicked Add", Toast.LENGTH_SHORT).show();
+            break;
+            case R.id.remove_item:
+            Toast.makeText(this, "you clicked Remove", Toast.LENGTH_SHORT).show();
+            break;
+            default:
+        }
+        return true;
+    }
+}//菜单
