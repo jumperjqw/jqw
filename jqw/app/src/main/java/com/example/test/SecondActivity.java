d57b4ad4bd340a28fa57c109ee16d8915974e38d
unknown <727199809@qq.com>
2019-05-06 21:15:23 +0800

    登陆跳转

---
+++ jqw/app/src/main/java/com/example/test/SecondActivity.java
@@ -0,0 +1,20 @@
+package com.example.test;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class SecondActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+        progressBar = (ProgressBar)findViewById(R.id.progressbar1);
+
+
+    }
+}
